28158a98cd479e8cb6143352fe3f9f7030c167c8
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-09-01 16:00:07 -0500

    Se agrego la entrada de datos por teclado.

--- 230831_S02_Ejercicios/src/programa/Principal.java
+++ 230831_S02_Ejercicios/src/programa/Principal.java
@@ -1,5 +1,6 @@
 package programa;
 
+import java.util.Scanner;
 import modelos.Pulgada;
 
 /**
@@ -11,21 +12,25 @@ public class Principal {
      * @param args son los argumentos de la línea de comandos.
      */
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         // =====[ Ejercicio 01 ]=====
+        System.out.print("Ingrese un valor en pulgadas: ");
+        double pulgadas = teclado.nextDouble();
         // Instanciando
-        double pulgadas = 504.5;
-        Pulgada p1 = new Pulgada(pulgadas);
+        System.out.println("-- Con Instancia --");
+        Pulgada plg = new Pulgada(pulgadas);
         System.out.println(pulgadas + " pulgadas equivalen a:");
-        System.out.println(p1.convertir_a_pies() + " pies");
-        System.out.println(p1.convertir_a_yardas()+ " yardas");
-        System.out.println(p1.convertir_a_metros() + " metros");
-        System.out.println(p1.convertir_a_centimetros() + " centimetros");
+        System.out.println(String.format("%.8f", plg.convertir_a_pies()) + " pies");
+        System.out.println(String.format("%.8f", plg.convertir_a_yardas()) + " yardas");
+        System.out.println(String.format("%.8f", plg.convertir_a_metros()) + " metros");
+        System.out.println(String.format("%.8f", plg.convertir_a_centimetros()) + " centimetros");
         // Sin instanciar
+        System.out.println("-- Sin Instancia --");
         System.out.println(pulgadas + " pulgadas equivalen a:");
-        System.out.println(Pulgada.convertir_a_pies(pulgadas) + " pies");
-        System.out.println(Pulgada.convertir_a_yardas(pulgadas) + " yardas");
-        System.out.println(Pulgada.convertir_a_metros(pulgadas) + " metros");
-        System.out.println(Pulgada.convertir_a_centimetros(pulgadas) + " centimetros");
+        System.out.println(String.format("%.8f", Pulgada.convertir_a_pies(pulgadas)) + " pies");
+        System.out.println(String.format("%.8f", Pulgada.convertir_a_yardas(pulgadas)) + " yardas");
+        System.out.println(String.format("%.8f", Pulgada.convertir_a_metros(pulgadas)) + " metros");
+        System.out.println(String.format("%.8f", Pulgada.convertir_a_centimetros(pulgadas)) + " centimetros");
         
         // =====[ Ejercicio 02 ]=====
     }
